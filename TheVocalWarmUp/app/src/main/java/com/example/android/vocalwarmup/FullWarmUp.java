a2b746ce177135a1d083655f65773bd892465203
Moonminky <noremori@gmail.com>
2017-09-16 16:33:57 +0200

    -Created classes for several activities -Created fields in WarmUpList layout and the corresponding intents to open new activities on click

---
+++ TheVocalWarmUp/app/src/main/java/com/example/android/vocalwarmup/FullWarmUp.java
@@ -0,0 +1,18 @@
+package com.example.android.vocalwarmup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Class for the full (long) warmup
+ */
+
+public class FullWarmUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fullwarmup);
+    }
+}
